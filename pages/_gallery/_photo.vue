1aaff0dbb652f58a4eaedac6c078f002b23062ff
flyc_chung <flyc_chung@paradise-soft.com.tw>
2017-11-08 17:23:53 +0800

    dynamic router works

---
+++ pages/_gallery/_photo.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+    	<h1>Photo Page</h1>
+        <p>photo number: <span v-text="photoNumber"></span></p>
+    </div>
+</template>
+
+<style scoped>
+    /* some scoped css */
+</style>
+
+<script>
+    import axios from "axios";
+
+    export default {
+        asyncData: function(context) {
+            return {
+                photoNumber: context.params.photo
+            }
+        },
+        data: function() {
+            return {
+                msg: "Context Params"
+            };
+        }
+    }
+</script>
---
+++ pages/_gallery/index.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+    	<h1>Gallery Page</h1>
+    </div>
+</template>
+
+<style scoped>
+    /* some scoped css */
+</style>
+
+<script>
+	export default {
+		layout: null,
+	    data: function() {
+	        return {
+
+	        };
+	    }
+	}
+</script>
