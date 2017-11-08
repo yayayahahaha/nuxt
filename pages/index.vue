c80d94422c05cca15d58b3d602e068a2914fde2e
flyc_chung <flyc_chung@paradise-soft.com.tw>
2017-11-08 15:17:48 +0800

    addd axios into package.json

--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,43 @@
 <template>
-	<div>
-		<p>hello</p>
-	</div>
+    <div>
+        <h1 v-text="msg"></h1>
+        <p v-for="item in context" v-text='item'></p>
+    </div>
 </template>
 
-<style></style>
+<style scoped>
+    /* some scoped css */
+</style>
 
 <script>
+    import axios from "axios";
 
-export default {
-    asyncData: function(context) {
-        return {
-            msg: Object.keys(context)
-        };
-    },
-    data: function() {
-        return {
-
-        };
+    export default {
+        asyncData: function(context) {
+            return axios({
+                method: 'get',
+                url: '/',
+                data: {
+                    key: "value"
+                },
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept':'application/json'
+                }
+            }).then(function(res) {
+                return {
+                    context: Object.keys(context)
+                };
+            }).catch(function(error) {
+                return {
+                    context: Object.keys(context)
+                };
+            });
+        },
+        data: function() {
+            return {
+                msg: "Context Params"
+            };
+        }
     }
-}
-
 </script>
