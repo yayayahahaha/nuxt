3da7a89847be206a8ac503b7ab368ea1e500a6cc
flyc_chung <flyc_chung@paradise-soft.com.tw>
2017-11-08 15:13:23 +0800

    very first commit of nuxt project

---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+	<div>
+		<p>hello</p>
+	</div>
+</template>
+
+<style></style>
+
+<script>
+
+export default {
+    asyncData: function(context) {
+        return {
+            msg: Object.keys(context)
+        };
+    },
+    data: function() {
+        return {
+
+        };
+    }
+}
+
+</script>
